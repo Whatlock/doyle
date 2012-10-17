fbfe96e917e7a7d7437f9fe15d4b24f4da90fb9d
Karl Brightman <karl@karlbright.org>
2012-10-17 15:14:38 +0800

    Moved generate functionality to model

--- app/controllers/stylesheets_controller.rb
+++ app/controllers/stylesheets_controller.rb
@@ -1,29 +1,8 @@
 class StylesheetsController < ApplicationController
 
-  LATEST_TEMPLATE_VERSION = '1.0'
-
-  TEMPLATE_PATH = 'app/assets/whatlock'
-  TEMPLATE_FILENAME = 'whatlock.css.sass.erb'
-  EXPORT_PATH = Rails.root.join 'public', 'stylesheets'
-
-  before_filter :get_stylesheet, only: [:edit,:update,:destroy,:compile]
-  before_filter :template_view_path
+  before_filter :get_stylesheet, only: [:edit,:update,:destroy,:generate]
 
   respond_to :html
-
-  def compile
-    @primary_color = @stylesheet.primary_color
-    @options = @stylesheet.options
-
-    FileUtils.mkdir_p exports_directory
-    File.open(target_stylesheet, 'w') {|f| f.write compiled }
-
-    @stylesheet.last_generated = DateTime.now
-    @stylesheet.url = target_stylesheet
-    @stylesheet.save
-
-    redirect_to stylesheets_path
-  end
 
   def index
     @stylesheets = Stylesheet.all
@@ -39,7 +18,13 @@ class StylesheetsController < ApplicationController
 
   def create
     @stylesheet = Stylesheet.create params[:stylesheet]
-    StylesheetGenerator.new(@stylesheet).delay.compile!
+    @stylesheet.delay.generate!
+    flash[:notice] = "Stylesheet will be generated soon..."
+    respond_with @stylesheet, location: stylesheets_path
+  end
+
+  def generate
+    @stylesheet.delay.generate!
     flash[:notice] = "Stylesheet will be generated soon..."
     respond_with @stylesheet, location: stylesheets_path
   end
@@ -62,32 +47,6 @@ class StylesheetsController < ApplicationController
 
   def get_stylesheet
     @stylesheet = Stylesheet.find params[:id]
-  end
-
-  def template_view_path
-    prepend_view_path TEMPLATE_PATH
-  end
-
-  def template_file
-    "#{@stylesheet.version}/#{TEMPLATE_FILENAME}"
-  end
-
-  def compiled
-    raw = render_to_string template: template_file, layout: nil
-    erb = ERB.new(raw).result(binding)
-    opts = Compass.configuration.to_sass_engine_options.merge syntax: :sass, style: :compressed
-    opts[:load_paths] << "#{Rails.root}/#{TEMPLATE_PATH}/#{@stylesheet.version}"
-    Sass::Engine.new(erb, opts).render
-  end
-
-  def exports_directory
-    EXPORT_PATH.join @stylesheet.version
-  end
-
-  def target_stylesheet
-    options = Digest::MD5.hexdigest(options.to_s)
-    filename = "doyle-#{@stylesheet.primary_color}-#{options}.css"
-    exports_directory.join filename
   end
 
 end
--- app/models/stylesheet.rb
+++ app/models/stylesheet.rb
@@ -1,11 +1,58 @@
 class Stylesheet < ActiveRecord::Base
+
+  LATEST_TEMPLATE_VERSION = '1.0'
+
+  TEMPLATE_PATH_ROOT = Rails.root.join 'app', 'assets', 'whatlock'
+  TEMPLATE_BASE_FILENAME = 'whatlock.css.sass.erb'
+  EXPORT_PATH_ROOT = Rails.root.join 'public', 'stylesheets'
+
   attr_accessible :author, :options, :primary_color, :version
 
-  before_save :set_version
+  validates_presence_of :primary_color
+
+  before_save :validate_primary_color
+
+  def generate
+    erb = ERB.new(template_file).result(binding)
+    opts = Compass.configuration.to_sass_engine_options.merge syntax: :sass, style: :compressed
+    opts[:load_paths] << Rails.root.join(template_path)
+    Sass::Engine.new(erb, opts).render
+  end
+
+  def generate!
+    FileUtils.mkdir_p export_path
+    File.open(export_target, 'w') { |f| f.write generate }
+    self.last_generated = DateTime.now
+    self.url = export_filename
+    save
+  end
 
   private
 
-  def set_version
-    self.version = "1.0"
+  def validate_primary_color
+    self.primary_color = "##{self.primary_color}" if self.primary_color[0] != '#'
   end
+
+  def template_path
+    "#{TEMPLATE_PATH_ROOT}/#{version}/"
+  end
+
+
+  def template_file
+    File.read(template_path + TEMPLATE_BASE_FILENAME)
+  end
+
+  def export_path
+    "#{EXPORT_PATH_ROOT}/#{version}/"
+  end
+
+  def export_filename
+    filename = Digest::MD5.hexdigest(self.primary_color + options.to_s)
+    "doyle#{filename}.css"
+  end
+
+  def export_target
+    export_path + export_filename
+  end
+
 end
