0fed9fbbaf58ed8f1985986b33c03366742d9908
Karl Brightman <karl@karlbright.org>
2012-10-19 16:32:33 +0800

    Updated s3 keys to env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],       # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],       # required
+    :aws_access_key_id      => ENV['aws_access_key_id'],       # required
+    :aws_secret_access_key  => ENV['aws_secret_access_key'],       # required
   }
-  config.fog_directory  = ENV['AWS_S3_BUCKET']
+  config.fog_directory  = ENV['aws_s3_bucket']
   config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
